85f2232acea61584b7da3d510c40ff5281c2f329
Adam Kacmarsky <akacmars@uwyo.edu>
2016-04-13 13:26:38 -0600

    Stimulation Update
    Error when committing last set.

--- Source/InputTest.m
+++ Source/InputTest.m
@@ -5,56 +5,61 @@
 %Plan is to use this matrix to train a neural net in the uninit script.
 %
 %Modified by: Adam
-%Last Modified: 12/4/16
+%Last Modified: 13/4/16
 
 function box_out = InputTest(box_in)
 
-    import java.awt.AWTException;
-    import java.awt.Robot;
-    import java.awt.event.KeyEvent;
-    import java.util.concurrent.TimeUnit;
+    %import java.awt.AWTException;
+    %import java.awt.Robot;
+    %import java.awt.event.KeyEvent;
+    %import java.util.concurrent.TimeUnit;
     
-    robot=Robot;
-    w = 0;
-    s = 0;
+    %robot=Robot;
+    
+    persistent w;
+    persistent s;    
     
     for i = 1: OV_getNbPendingInputChunk(box_in,2)
 		
 		% Pop the first stimulation chunk (2 denotes stimulations)
 		[box_in, start_time, end_time, stim_set] = OV_popInputBuffer(box_in,2);
 		
-		% the stimulation set is a 3xN matrix.
-		% The openvibe stimulation stream sends even empty stimulation set
-		% so the following boxes know there is no stimulation to expect in
-		% the latest time range. These empty chunk are also in the matlab buffer.
-		if(numel(stim_set) >= 3) % at least one stim in the set.
+        %stim_set is a 3x1 matrix, where the first value is the value of
+        %the label sent by the simulation. If there is at least one
+        %stimulation in the set, run the keyboar handler.
+		if(numel(stim_set) >= 3)
+            %33205 is the value of stimulation label 01. 
             if stim_set(1) == 33025
-                disp(stim_set(1));
                 w = 1;
+                %switch a trigger so that the chunk of data is handled
+                %later
                 box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
             end
             
+            %33026 is the value of stimulation label 02
             if stim_set(1) == 33026
                 s = 1;
                 box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
             end
         end
-	end
+    end
 
+    %code for handling chunks of data. Each chunk should only be handled if
+    %the trigger is true (stimulation has been processed).
     for i = 1: OV_getNbPendingInputChunk(box_in,1)
         [box_in, start_time, end_time, matrix_data] = OV_popInputBuffer(box_in,1);   
         
         box_in.outputs{1}.header = box_in.inputs{1}.header;
         
+        %if trigger is true, push data chunk to new data matrix (x). Push
+        %associated stimulation chunk to output matrix (t).
         if (box_in.user_data.trigger_state)
             box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
-            disp(box_in.user_data.trigger_state);
             disp(w);
             if (w == 1)
                 disp(matrix_data);
             end
         end
-            
         
         %{
         if (average > 0)
@@ -67,8 +72,8 @@ function box_out = InputTest(box_in)
             robot.keyRelease(KeyEvent.VK_RIGHT);
             robot.keyRelease(KeyEvent.VK_LEFT);
         end
-        %} 
-    
+        %}
+        
     end
     
     box_out = box_in;
