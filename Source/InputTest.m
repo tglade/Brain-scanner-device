e6759557a4443cba0d05e08dd5f72f831a0f1612
Adam Kacmarsky <akacmars@uwyo.edu>
2016-04-13 12:36:37 -0600

    Beginning Neural Net Code
    Updated the xml and test codes to begin to form a training set for a
    neural net.  Also added keyboard_stim.txt to handle keyboard
    stimulations.

--- Source/InputTest.m
+++ Source/InputTest.m
@@ -1,3 +1,11 @@
+%Modified from tuto1_signal_filer_Process.m
+%
+%Currently attempting to handle two inputs, and map the corresponding chunk
+%to a new matrix, which contains the chunk and the appropriate keypress.
+%Plan is to use this matrix to train a neural net in the uninit script.
+%
+%Modified by: Adam
+%Last Modified: 12/4/16
 
 function box_out = InputTest(box_in)
 
@@ -7,22 +15,48 @@ function box_out = InputTest(box_in)
     import java.util.concurrent.TimeUnit;
     
     robot=Robot;
+    w = 0;
+    s = 0;
+    
+    for i = 1: OV_getNbPendingInputChunk(box_in,2)
+		
+		% Pop the first stimulation chunk (2 denotes stimulations)
+		[box_in, start_time, end_time, stim_set] = OV_popInputBuffer(box_in,2);
+		
+		% the stimulation set is a 3xN matrix.
+		% The openvibe stimulation stream sends even empty stimulation set
+		% so the following boxes know there is no stimulation to expect in
+		% the latest time range. These empty chunk are also in the matlab buffer.
+		if(numel(stim_set) >= 3) % at least one stim in the set.
+            if stim_set(1) == 33025
+                disp(stim_set(1));
+                w = 1;
+                box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
+            end
+            
+            if stim_set(1) == 33026
+                s = 1;
+                box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
+            end
+        end
+	end
 
     for i = 1: OV_getNbPendingInputChunk(box_in,1)
+        [box_in, start_time, end_time, matrix_data] = OV_popInputBuffer(box_in,1);   
         
-        [box_in, start_time, end_time, matrix_data] = OV_popInputBuffer(box_in,1);
+        box_in.outputs{1}.header = box_in.inputs{1}.header;
         
-        L = box_in.inputs{1}.header.nb_samples_per_buffer;
-        sum = 0;
-        
-        for j = 1:L
-           
-            sum = sum + matrix_data(j);
-            
+        if (box_in.user_data.trigger_state)
+            box_in.user_data.trigger_state = ~box_in.user_data.trigger_state;
+            disp(box_in.user_data.trigger_state);
+            disp(w);
+            if (w == 1)
+                disp(matrix_data);
+            end
         end
+            
         
-        average = sum / L;
-        
+        %{
         if (average > 0)
             robot.keyRelease(KeyEvent.VK_LEFT);
             robot.keyPress(KeyEvent.VK_RIGHT);
@@ -33,8 +67,7 @@ function box_out = InputTest(box_in)
             robot.keyRelease(KeyEvent.VK_RIGHT);
             robot.keyRelease(KeyEvent.VK_LEFT);
         end
-        
-        TimeUnit.SECONDS.sleep(2);
+        %} 
     
     end
     
--- Source/InputTest_Uninitialize.m
+++ Source/InputTest_Uninitialize.m
@@ -6,8 +6,8 @@ function box_out = InputTest_Uninitialize(box_in)
 	disp('Matlab uninitialize function has been called.')
     robot = Robot;
     
-    robot.KeyRelease(KeyEvent.VK_RIGHT);
-    robot.KeyRelease(KeyEvent.VK_LEFT);
+    %robot.KeyRelease(KeyEvent.VK_RIGHT);
+    %robot.KeyRelease(KeyEvent.VK_LEFT);
 
 	box_out = box_in;
 end
